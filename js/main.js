e50784957fdc7a6cbdc3055e5f59fdceb1b3a41b
keyztrokee <raymondfajiculay.dev@gmail.com>
2022-09-25 07:05:54 +0800

    finish challenge

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const submitBtn = document.querySelector(".btnSubmit");
+const resultContainer = document.querySelector(".result");
+const ratingContainer = document.querySelector(".rating");
+const choices = document.querySelectorAll(".choices");
+const rateResult = document.querySelector("#rateResult");
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        console.log(choice.innerHTML);
+        rateResult.innerHTML = choice.innerHTML;
+
+        submitBtn.addEventListener("click", () => {
+            ratingContainer.classList.add("hidden");
+            resultContainer.classList.remove("hidden");
+        });
+    });
+});
